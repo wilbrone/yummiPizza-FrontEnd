fdd3585f96c878f7acf6145b167d9a8cb88fc506
wilbrone <wilbroneokoth@gmail.com>
2020-05-03 13:20:52 +0300

    add immages to the assests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 import { HomeComponent } from './components/home/home.component';
 import { OrderComponent } from './components/order/order.component';
@@ -32,7 +33,8 @@ import { OrderComponent } from './components/order/order.component';
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
